2b0bebbbfbbc07515534ed51efd71f7293038971
Osamayy515 <osamasaleem515@gmail.com>
2022-02-21 18:51:14 +0500

    Run migrations

---
+++ playground/views.py
@@ -0,0 +1,13 @@
+from re import X
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def calculate():
+    x=1
+    y=2
+    return X
+
+def say_hello(request):
+    x= calculate()
+    return render(request, 'hello.html', {'name':'Osama'})
