232eb46e482568120551aaf54aa194dc99189f57
Osamayy515 <osamasaleem515@gmail.com>
2022-02-26 18:32:51 +0500

    managers and querysets

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,18 @@
 from re import X
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
+
 
 # Create your views here.
-def calculate():
-    x=1
-    y=2
-    return X
-
 def say_hello(request):
-    x= calculate()
+    query_set = Product.objects.all()
+
+    for product in query_set:
+        print(product)
+
+    # list(query_set)
+    # query_set[0:5]
+    # query_set.filter().filter().order_by()
+
     return render(request, 'hello.html', {'name':'Osama'})
